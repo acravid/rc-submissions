1b79ceabc476483691b5a181569ef1d3b209a5fd
Allan Fernandes <allancravid@tecnico.ulisboa.pt>
2022-12-13 03:05:52 +0000

    common: implement generic like buffer structure

--- src/common/common.h
+++ src/common/common.h
@@ -19,11 +19,26 @@
 struct addrinfo *getaddrinfo_extended(const char *ip, const char *port, int family, int socktype,int  protocol);
 
 
+// Function Prototypes 
 
+
+
+// Macros 
 #define AUTO_PROTOCOL 0
 #define SUCCEEDED 0
 
+// Macros for creating and manipulating buffers (generic buffers)
 
+#define buffer_typedef(type,typename) \
+    typedef struct {                  \
+        size_t size;                  \
+        type* info;                   \
+    }typename;                        \
+
+#define buffer_init(buf,len,type) \
+    type info[len]; \
+    buf.size = len; \
+    buf.info = &info; 
 
 
 
